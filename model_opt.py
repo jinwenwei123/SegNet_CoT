b96bc7d44fd7fe0fe26db2a50c9b24192f6d0751
AshHikari <74419420+jinwenwei123@users.noreply.github.com>
2025-01-15 17:03:42 +0800

    Update model_opt.py

--- model_opt.py
+++ model_opt.py
@@ -9,7 +9,7 @@ model_opt = {
     "num_classes": 20 + 1,  # 类别数 + 背景
     "batch_size": 6,  # 样本簇大小
     "lr": 0.001,  # 学习率
-    "epoch": 1000,  # 训练轮数
+    "epoch": 5,  # 训练轮数
     "root": "./dataset",  # 数据集根目录
     "pretrain": True,  # 是否启用预训练
     "weight_save_path": "./weight",  # 权重文件目录
